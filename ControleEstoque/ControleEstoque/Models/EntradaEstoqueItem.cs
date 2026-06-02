844f60c1ea3602f800cedc9566a6eb5e6f192ec5
JuniorPastorini <juniorpastorini_@hotmail.com>
2026-06-01 23:14:35 -0300

    ajustando modais

--- ControleEstoque/ControleEstoque/Models/EntradaEstoqueItem.cs
+++ ControleEstoque/ControleEstoque/Models/EntradaEstoqueItem.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ControleEstoque.Models
 {
     public class EntradaEstoqueItem
@@ -16,8 +18,10 @@ namespace ControleEstoque.Models
         // DADOS
         public decimal Quantidade { get; set; }
 
+        [Column(TypeName = "decimal(18,5)")]
         public decimal ValorCusto { get; set; }
 
+        [Column(TypeName = "decimal(18,5)")]
         public decimal Subtotal { get; set; }
     }
 }
